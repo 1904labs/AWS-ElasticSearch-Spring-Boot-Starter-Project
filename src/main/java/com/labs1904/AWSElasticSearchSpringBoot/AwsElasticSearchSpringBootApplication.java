85ae70ddcf9bbafaa84db9e9e9537081af04d5b6
Kyle Smith <kyle.smith@1904labs.com>
2018-06-14 15:50:10 -0500

    Initial commit

---
+++ src/main/java/com/labs1904/AWSElasticSearchSpringBoot/AwsElasticSearchSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.labs1904.AWSElasticSearchSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticSearchSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticSearchSpringBootApplication.class, args);
+	}
+}
